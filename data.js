bd5f9164a5c940c92d7e445f8173e801267ab3b3
Marlon Lima <marlonlima984@gmail.com>
2024-09-04 08:18:57 -0300

    Added the files for future javascript implementation and a few tweeks on the css on the page to improve on small screen sizes

---
+++ data.js
@@ -0,0 +1,23 @@
+let dados = [
+  {
+    nome: "Mimikyu",
+    id: 778,
+    descrição:
+      "Este Pokémon vive em lugares escuros intocados pela luz do sol. Quando aparece diante dos humanos, ele se esconde sob um pano que lembra um Pikachu.",
+    link: "https://www.pokemon.com/br/pokedex/mimikyu",
+  },
+  {
+    nome: "Drampa",
+    id: 785,
+    descrição:
+      "As montanhas que ele chama de lar têm quase duas milhas de altura. Em raras ocasiões, ele desce para brincar com as crianças que vivem nas cidades abaixo.",
+    link: "https://www.pokemon.com/br/pokedex/drampa",
+  },
+  {
+    nome: "Salamence",
+    id: 373,
+    descrição:
+      "Ele voa em suas asas, que cresceram finalmente. Em sua felicidade, ele jorra chamas quentes, queimando tudo que passa.",
+    link: "https://www.pokemon.com/br/pokedex/salamence",
+  }
+];
