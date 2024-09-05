4ce053710567bf38fe5c36d808a0c91ce391bbbc
Marlon Lima <marlonlima984@gmail.com>
2024-09-05 08:58:35 -0300

    Added js to generate the search results dynamically and take the data from the dataset, added better formatition for the search result cards and changed the image in the page for a Non-copyrigthed version

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log(dados[0]);
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+
+    // Itera sobre cada elemento (dado) do array de dados
+    for (let dado of dados) {
+        // Constrói o HTML para cada resultado, formatando os dados do objeto
+        resultados += `
+            <div class="item-resultado">
+                <h2>${dado.nome}</h2>
+                <p> 
+                    <strong>id:</strong> ${dado.id}<br>
+                    <strong>tipo:</strong> ${dado.tipo}<br>
+                    <strong>Descrição:</strong> ${dado.desc}
+                </p>
+                <a href="https://www.pokemon.com/br/pokedex/${dado.nome}" target="_blank">Mais informações</a>
+            </div>
+        `;
+    }
+
+    // Atribui o HTML gerado à seção de resultados, atualizando a página
+    section.innerHTML = resultados;
+}
+
--- data.js
+++ data.js
@@ -2,21 +2,24 @@ let dados = [
   {
     nome: "Mimikyu",
     id: 778,
-    descrição:
+    tipo: "fantasma, fada",
+    desc:
       "Este Pokémon vive em lugares escuros intocados pela luz do sol. Quando aparece diante dos humanos, ele se esconde sob um pano que lembra um Pikachu.",
     link: "https://www.pokemon.com/br/pokedex/mimikyu",
   },
   {
     nome: "Drampa",
     id: 785,
-    descrição:
+    tipo: "Dragão, Normal",
+    desc:
       "As montanhas que ele chama de lar têm quase duas milhas de altura. Em raras ocasiões, ele desce para brincar com as crianças que vivem nas cidades abaixo.",
     link: "https://www.pokemon.com/br/pokedex/drampa",
   },
   {
     nome: "Salamence",
     id: 373,
-    descrição:
+    tipo: "Dragão, Voador",
+    desc:
       "Ele voa em suas asas, que cresceram finalmente. Em sua felicidade, ele jorra chamas quentes, queimando tudo que passa.",
     link: "https://www.pokemon.com/br/pokedex/salamence",
   }
